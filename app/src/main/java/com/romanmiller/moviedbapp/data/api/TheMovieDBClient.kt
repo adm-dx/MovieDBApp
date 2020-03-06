6580e85ac827a7eba21667a92a135337f9b2affb
Roman Hohlov <adm.dx@outlook.com>
2020-03-07 00:37:59 +0300

    Starting work for main activity

--- app/src/main/java/com/romanmiller/moviedbapp/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/romanmiller/moviedbapp/data/api/TheMovieDBClient.kt
@@ -11,6 +11,9 @@ const val API_KEY = "ebf0381b28ba8fa67f2d1d133098e760"
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342/"
 
+const val FIRST_PAGE = 1
+const val POST_PER_PAGE = 20
+
 object TheMovieDBClient {
 
     fun getClient(): TheMovieDBInterface {
--- app/src/main/java/com/romanmiller/moviedbapp/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/com/romanmiller/moviedbapp/data/api/TheMovieDBInterface.kt
@@ -1,15 +1,20 @@
 package com.romanmiller.moviedbapp.data.api
 
 import com.romanmiller.moviedbapp.data.vo.MovieDetails
+import com.romanmiller.moviedbapp.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
 
     // https://api.themoviedb.org/3/movie/popular?api_key=ebf0381b28ba8fa67f2d1d133098e760
     // https://api.themoviedb.org/3/movie/475303?api_key=ebf0381b28ba8fa67f2d1d133098e760
     // https://api.themoviedb.org/3/
+
+    @GET("movie/popular")
+    fun getPopularMovie(@Query("page") page: Int): Single<MovieResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails(@Path("movie_id") id: Int): Single<MovieDetails>
